9fa043ea1c8d6719b98c6ce59373f63344b02810
Aditya Das <adityadas99906@gmail.com>
2025-09-20 20:16:31 +0530

    style

--- src/components/ButtonsContainer.jsx
+++ src/components/ButtonsContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./ButtonsContainer.module.css";
 
-const ButtonsContainer = ({onButtonClick}) => {
+const ButtonsContainer = ({ onButtonClick }) => {
   const buttonsName = [
     "C",
     "1",
@@ -25,8 +25,14 @@ const ButtonsContainer = ({onButtonClick}) => {
   return (
     <div>
       <div className={styles.buttonsContainer}>
-        {buttonsName.map((item) => (
-          <button className={styles.button} onClick={ ()=> onButtonClick(item)} > {item} </button>
+        {buttonsName.map((item, index) => (
+          <button
+            key={index}
+            className={styles.button}
+            onClick={() => onButtonClick(item)}
+          >
+            {item}
+          </button>
         ))}
       </div>
     </div>
